6ee2a37ceeae48fa0733b1c0306648ce3308c45c
Nicolas Housset <nicolas.housset@gmail.com>
2021-07-27 11:01:24 +0200

    Update analyseLogCas.sh

--- analyseLogCas.sh
+++ analyseLogCas.sh
@@ -9,8 +9,10 @@ export logCasCtrl=cas_2021-07-26_lp30022.posix.covea.priv_129846.log
 
 grep $casUserName $logPath/$logCasCtrl > /tmp/$$.casCtrl
 
-export TackingSessionCtrl=$(grep "Tracking Session:" /tmp/$$.casCtrl)
+export TackingSessionCtrl=$(grep "Tracking Session:" /tmp/$$.casCtrl | head -1 |cut -d "/" -f 1)
 echo $TackingSessionCtrl
+
+
 
 
 
