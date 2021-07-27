a8077957c6b8e8ed65eb9f7d39ef66ef491f8353
Nicolas Housset <nicolas.housset@gmail.com>
2021-07-27 11:06:31 +0200

    Update analyseLogCas.sh

--- analyseLogCas.sh
+++ analyseLogCas.sh
@@ -9,13 +9,17 @@ export logCasCtrl=cas_2021-07-26_lp30022.posix.covea.priv_129846.log
 
 grep $casUserName $logPath/$logCasCtrl > /tmp/$$.casCtrl
 
+export startSession=$( grep "launchServicesLaunch" | cut -d " " -f 1)
+echo $startSession
+
+
 export TackingSessionCtrl=$(grep "Tracking Session:" /tmp/$$.casCtrl | head -1 |cut -d "/" -f 1)
 echo $TackingSessionCtrl
 
 export TackingSessionCtrlID=$(echo $TackingSessionCtrl | awk -F " " '{print $NF}' )
 echo $TackingSessionCtrlID
 
-grep $TackingSessionCtrlID /tmp/$$.casCtrl > /tmp/$$.$TackingSessionCtrlIDc.casCtrl
+grep $TackingSessionCtrlID /tmp/$$.casCtrl > /tmp/$$.$TackingSessionCtrlID.casCtrl
 
 
 
