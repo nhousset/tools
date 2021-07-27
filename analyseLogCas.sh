9bcd708f35e5fb9c807923dca9c446c4a44a3d24
Nicolas Housset <nicolas.housset@gmail.com>
2021-07-27 14:04:16 +0200

    Update analyseLogCas.sh

--- analyseLogCas.sh
+++ analyseLogCas.sh
@@ -68,7 +68,7 @@ echo -en "${BLUE} Start ${NC}\n"
 
 echo -en "${RED} Tracking Session ${NC}\n"
 
-for sessionId in $(cat /tmp/$$.casCtrl  | grep 'session' | grep 'Process ID'  | awk '{print $1"|"$15}')
+for sessionId in $(cat /tmp/$$.casCtrl  | grep 'session' | grep 'Process ID'  | awk '{print $1"|"$15}' | sed s/"."/""/g )
 do
 	echo $sessionId
 done
