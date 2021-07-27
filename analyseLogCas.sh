1742fab66a552f47f13cea7d69b24cb12e934b36
Nicolas Housset <nicolas.housset@gmail.com>
2021-07-27 12:10:23 +0200

    Update analyseLogCas.sh

--- analyseLogCas.sh
+++ analyseLogCas.sh
@@ -66,8 +66,10 @@ echo -en "${BLUE} Start ${NC}\n"
 #export startSession=$( grep "launchServicesLaunch"  /tmp/$$.casCtrl | cut -d " " -f 1 | head -1)
 #echo $startSession
 
-echo -en "${RED} Tracking Session ${NC}\n"
-grep " Launched session controllers" /tmp/$$.casCtrl 
+echo -en "${RED} Tracking Session ${NC}\n"/tmp/$$.casCtrl 
+set +x
+cat /tmp/$$.casCtrl  | grep "Launched session controllers"
+set -x
 #| cut -d "/" -f 1
 
 exit;
